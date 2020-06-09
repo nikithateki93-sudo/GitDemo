50c5c10b8a781b79a98f20cbe5e2ff26f716b835
nikitha <nikithateki10@gmail.com>
2020-06-09 12:19:20 -0500

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void welcome()
+    {
+        System.out.println("Welcome");
+    }
+    @Test
+    public void hru()
+    {
+        System.out.println("H r u");
+    }
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+//import org.junit.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserautomation() {
+		System.out.println("Hello");
+
+	}
+
+	@Test
+	public void UI() {
+
+		System.out.println("Bye");
+	}
+
+}
